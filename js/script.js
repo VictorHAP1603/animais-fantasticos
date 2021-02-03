6002d8d9a793d9cfdd6d2058fb9cd8d2899f56da
Victor Hugo <valvesperricci@yahoo.com.br>
2021-02-03 03:39:07 -0300

    fetchanimais-refatorado

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ import Accordion from "./modules/accordion-list.js";
 import TabNav from "./modules/tab-nav.js";
 import Modal from "./modules/modal.js";
 import ToolTip from "./modules/tooltip.js";
+import initFetchAnimais from "./modules/fetch-animais.js";
 import initScrollWindow from "./modules/scroll-window.js";
 import { initShowMenu, initRemoveMenu } from "./modules/menu-mobile.js";
 import initDropDown from "./modules/dropdown.js";
 import initDate from "./modules/date.js";
-import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
 const scrollsuave = new ScrollSuave("[data-menu]");
@@ -32,10 +32,11 @@ modal.init();
 const toolTip = new ToolTip("[data-tooltip]");
 toolTip.init();
 
+initFetchAnimais("../../animaisapi.json", ".numeros-grid");
+
 initScrollWindow();
 initShowMenu();
 initRemoveMenu();
 initDropDown();
 initDate();
-initFetchAnimais();
 initFetchBitcoin();
